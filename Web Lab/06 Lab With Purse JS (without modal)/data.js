62f78bf655ea9a80e0e2535cf392f85fb7fd7c1f
saimbintalib <49138179+saimbintalib@users.noreply.github.com>
2020-11-29 08:41:07 +0500

    Add files via upload

---
+++ Web Lab/06 Lab With Purse JS (without modal)/data.js
@@ -0,0 +1,55 @@
+//data.js
+let students = [
+    {
+        name: "Bilal Ali",
+        rollNo: "bcsf16a030",
+        degree: "BS.CS",
+        semester: "7",
+        subjects: [
+            {name: "Programming", grade: "A"},
+            {name: "Discrete Math", grade: "A-"},
+            {name: "OOP", grade: "A"}
+
+        ]
+    },
+    {
+        name: "Maira Akram",
+        rollNo: "bcsf16a019",
+        degree: "BS.CS",
+        semester: "8",
+        subjects: [
+            {name: "Programming", grade: "C"},
+            {name: "Discrete Math", grade: "A"},
+            {name: "OOP", grade: "C+"}
+
+        ]
+    },
+    {
+        name: "Hasham Sabir",
+        rollNo: "bcsf16a042",
+        degree: "BS.CS",
+        semester: "7",
+        subjects: [
+            {name: "Programming", grade: "C"},
+            {name: "Discrete Math", grade: "A"},
+            {name: "OOP", grade: "C+"},
+            {name: "Computer Networks", grade: "B+"},
+            {name: "Statistics", grade: "F"}
+
+        ]
+    },
+    {
+        name: "Testing Name",
+        rollNo: "Testing RollNo",
+        degree: "Testing Degree",
+        semester: "Testing Semester",
+        subjects: [
+            {name: "Subject 1", grade: "A"},
+            {name: "Subject 2", grade: "C-"},
+            {name: "Subject 3", grade: "B+"},
+            {name: "Subject 4", grade: "D"},
+            {name: "Subject 5", grade: "B"}
+
+        ]
+    }
+]
